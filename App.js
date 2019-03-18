a62b13e50192791223dafb4342961113326e1f92
osdnk <micosa97@gmail.com>
2019-03-18 20:22:01 +0100

    future

--- App.js
+++ App.js
@@ -1,573 +1,81 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, TouchableWithoutFeedback, Dimensions, Text, Platform, Image } from 'react-native';
-import {
-  DangerZone, GestureHandler
-} from 'expo';
+import React from 'react';
+import { Text, View, FlatList, StyleSheet, TouchableOpacity, YellowBox } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-const { Animated } = DangerZone;
-const {
-  PanGestureHandler,
-  TapGestureHandler,
-  State,
-} = GestureHandler;
+import Map from './Map';
 
+const SCREENS = {
+  map: {
+    screen: Map,
+    title: 'Apple Map',
+  },
+};
 
-const { height } = Dimensions.get('window');
-
-
-const P = (android, ios) => Platform.OS === 'ios' ? ios : android;
-
-const magic = {
-  damping: P(9, 7),
-  mass: 0.3,
-  stiffness: 121.6,
-  overshootClamping: true,
-  restSpeedThreshold: 0.1,
-  restDisplacementThreshold: 0.1,
-  deceleration: 0.999,
-  bouncyFactor: 1,
-  velocityFactor: P(1, 1.2),
-  dampingForMaster: 50,
-  tossForMaster: 0.4,
-  coefForTranslatingVelocities: 5
-
-}; // pls do it better
-
-const {
-  damping,
-  dampingForMaster,
-  mass,
-  stiffness,
-  overshootClamping,
-  restSpeedThreshold,
-  restDisplacementThreshold,
-  deceleration,
-  velocityFactor,
-  tossForMaster
-} = magic;
-
-
-const { set, cond, onChange, block, eq, greaterOrEq, not, defined, max, add, and, Value, spring, or, divide, greaterThan, sub, event, diff, multiply, clockRunning, startClock, stopClock, decay, Clock, lessThan } = Animated;
-
-
-function runDecay(clock, value, velocity, wasStartedFromBegin) {
-  const state = {
-    finished: new Value(0),
-    velocity: new Value(0),
-    position: new Value(0),
-    time: new Value(0),
+class MainScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Bottom Sheet Examples',
   };
-
-  const config = { deceleration };
-
-  return [
-    cond(clockRunning(clock), 0, [
-      cond(wasStartedFromBegin, 0, [
-        set(wasStartedFromBegin, 1),
-        set(state.finished, 0),
-        set(state.velocity, multiply(velocity, velocityFactor)),
-        set(state.position, value),
-        set(state.time, 0),
-        startClock(clock),
-      ]),
-    ]),
-    cond(clockRunning(clock), decay(clock, state, config)),
-    cond(state.finished, stopClock(clock)),
-    state.position,
-  ];
-}
-
-
-function withPreservingAdditiveOffset(drag, state) {
-  const prev = new Animated.Value(0);
-  const valWithPreservedOffset = new Animated.Value(0);
-  return block([
-    cond(eq(state, State.BEGAN), [
-      set(prev, 0)
-    ], [
-      set(valWithPreservedOffset, add(valWithPreservedOffset, sub(drag, prev))),
-      set(prev, drag),
-    ]),
-    valWithPreservedOffset
-  ]);
-}
-
-function withDecaying(drag, state, decayClock, velocity, prevent) {
-  const valDecayed = new Animated.Value(0);
-  const offset = new Animated.Value(0);
-  // since there might be moar than one clock
-  const wasStartedFromBegin = new Animated.Value(0);
-  return block([
-    cond(eq(state, State.END),
-      [
-        cond(prevent,
-          stopClock(decayClock),
-          set(valDecayed, runDecay(decayClock, add(drag, offset), velocity, wasStartedFromBegin))
-        )
-      ],
-      [
-        stopClock(decayClock),
-        cond(eq(state, State.BEGAN, set(prevent, 0))),
-        cond(eq(state, State.BEGAN), [
-          set(wasStartedFromBegin, 0),
-          set(offset, add(sub(valDecayed, drag)))
-        ]),
-        set(valDecayed, add(drag, offset))
-      ],
-    ),
-    valDecayed,
-  ]);
-}
-
-
-function runSpring(clock, value, velocity, dest, damping = damping, wasRun = 0, isManuallySet = 0) {
-  const state = {
-    finished: new Value(0),
-    velocity: new Value(0),
-    position: new Value(0),
-    time: new Value(0),
-  };
-
-  const config = {
-    damping,
-    mass,
-    stiffness,
-    overshootClamping,
-    restSpeedThreshold,
-    restDisplacementThreshold,
-    toValue: new Value(0),
-  };
-
-  return [
-    cond(clockRunning(clock), 0, [
-      set(state.finished, 0),
-      set(state.velocity, velocity),
-      set(state.position, value),
-      set(config.toValue, dest),
-      cond(and(wasRun, not(isManuallySet)), 0, startClock(clock)),
-      cond(defined(wasRun), set(wasRun, 1)),
-    ]),
-    spring(clock, state, config),
-    cond(state.finished, stopClock(clock)),
-    state.position,
-  ];
-}
-
-class BottomSheetBehavior extends Component {
-  static defaultProps = {
-    initialSnap: 0,
-    enabledManualSnapping: true,
-    enabledGestureInteraction: true,
-  };
-
-  decayClock = new Clock();
-  panState = new Value(0);
-  tapState = new Value(0);
-  velocity = new Value(0);
-  panMasterState = new Value(State.END);
-  masterVelocity = new Value(0);
-  isManuallySetValue = new Animated.Value(0);
-  manuallySetValue = new Animated.Value(0);
-  masterClockForOverscroll = new Clock();
-  preventDecaying = new Animated.Value(0);
-  dragMasterY = new Value(0);
-  dragY = new Value(0);
-  constructor(props) {
-    super(props);
-    this.state = BottomSheetBehavior.getDerivedStateFromProps(props);
-    const { snapPoints, init } = this.state;
-    const middlesOfSnapPoints = [];
-    for (let i = 1; i < snapPoints.length; i++) {
-      middlesOfSnapPoints.push(divide(add(snapPoints[i - 1], snapPoints[i]), 2));
-    }
-    const masterOffseted =
-      new Value(init);
-    // destination point is a approximation of movement if finger released
-    const destinationPoint = add(masterOffseted, multiply(tossForMaster, this.masterVelocity));
-    // method for generating condition for finding the nearest snap point
-    const currentSnapPoint = (i = 0) => i + 1 === snapPoints.length ?
-      snapPoints[i] :
-      cond(
-        lessThan(destinationPoint, middlesOfSnapPoints[i]),
-        snapPoints[i],
-        currentSnapPoint(i + 1)
-      );
-    // current snap point desired
-    this.snapPoint = currentSnapPoint();
-
-    const masterClock = new Clock();
-    const prevMasterDrag = new Animated.Value(0);
-    const wasRun = new Animated.Value(0);
-    this.translateMaster = block([
-      cond(eq(this.panMasterState, State.END),
-        [
-          set(prevMasterDrag, 0),
-          cond(or(clockRunning(masterClock), not(wasRun), this.isManuallySetValue),
-            [
-              cond(this.isManuallySetValue, stopClock(masterClock)),
-              set(masterOffseted,
-                runSpring(masterClock, masterOffseted, this.masterVelocity,
-                  cond(this.isManuallySetValue, this.manuallySetValue, this.snapPoint),
-                  dampingForMaster, wasRun, this.isManuallySetValue)
-              ),
-              set(this.isManuallySetValue, 0)
-            ]
-          ),
-        ],
-        [
-          stopClock(masterClock),
-          set(this.preventDecaying, 1),
-          set(masterOffseted, add(masterOffseted, sub(this.dragMasterY, prevMasterDrag))),
-          set(prevMasterDrag, this.dragMasterY),
-          cond(eq(this.panMasterState, State.BEGAN),
-            [
-              stopClock(this.masterClockForOverscroll),
-              set(wasRun, 0),
-            ]
-          ),
-        ]
-      ),
-      max(masterOffseted, snapPoints[0])
-    ]);
-
-    this.Y = this.withEnhancedLimits(
-      withDecaying(
-        withPreservingAdditiveOffset(this.dragY, this.panState),
-        this.panState,
-        this.decayClock,
-        this.velocity,
-        this.preventDecaying),
-      masterOffseted);
-  }
-
-  handleMasterPan = event([{ nativeEvent: ({
-    translationY: this.dragMasterY,
-    state: this.panMasterState,
-    velocityY: this.masterVelocity
-  })}]);
-
-  handlePan = event([{ nativeEvent: ({
-    translationY: this.dragY,
-    state: this.panState,
-    velocityY: this.velocity
-  })}]);
-
-  handleTap = event([{ nativeEvent: { state: this.tapState } }]);
-
-  withEnhancedLimits(val, masterOffseted) {
-    const wasRunMaster = new Animated.Value(0)
-    const min = multiply(-1, add(this.state.heightOfContent, this.state.heightOfHeaderAnimated))
-    const prev = new Animated.Value(0);
-    const limitedVal = new Animated.Value(0);
-    const diffPres = new Animated.Value(0);
-    const flagWasRunSpring = new Animated.Value(0);
-    const justEndedIfEnded = new Animated.Value(1);
-    const wasEndedMasterAfterInner = new Animated.Value(1);
-    const prevMaster = new Animated.Value(1);
-    const prevState = new Animated.Value(0);
-
-    const rev = new Animated.Value(0);
-    return block([
-      set(rev, limitedVal),
-      cond(or(eq(this.panState, State.BEGAN), and(eq(this.panState, State.ACTIVE), eq(prevState, State.END))), [
-        set(prev, val),
-        set(flagWasRunSpring, 0),
-        stopClock(this.masterClockForOverscroll),
-        set(wasRunMaster, 0),
-      ], [
-        set(limitedVal, add(limitedVal, sub(val, prev))),
-        cond(lessThan(limitedVal, min), set(limitedVal, min)),
-      ]),
-      set(prevState, this.panState), // some iOS shit
-      set(diffPres, sub(prev, val)),
-      set(prev, val),
-      cond(or(greaterOrEq(limitedVal, 0),
-        greaterThan(masterOffseted, 0))
-        , [
-          cond(eq(this.panState, State.ACTIVE),
-            set(masterOffseted, sub(masterOffseted, diffPres)),
-          ),
-          cond(greaterThan(masterOffseted, 0), [
-            set(limitedVal, 0)
-          ]),
-          cond(not(eq(this.panState, State.END)), set(justEndedIfEnded, 1)),
-          cond(or(eq(this.panState, State.ACTIVE), eq(this.panMasterState, State.ACTIVE)), set(wasEndedMasterAfterInner, 0)),
-          cond(and(eq(prevMaster, State.ACTIVE), eq(this.panMasterState, State.END), eq(this.panState, State.END)), set(wasEndedMasterAfterInner, 1)),
-          set(prevMaster, this.panMasterState),
-          cond(and(eq(this.panState, State.END), not(wasEndedMasterAfterInner), not(eq(this.panMasterState, State.ACTIVE)), not(eq(this.panMasterState, State.BEGAN)), or(clockRunning(this.masterClockForOverscroll), not(wasRunMaster))), [
-           // cond(justEndedIfEnded, set(this.masterVelocity, diff(val))),
-            set(this.masterVelocity, cond(justEndedIfEnded, diff(val), this.velocity)),
-            set(masterOffseted, runSpring(this.masterClockForOverscroll, masterOffseted, diff(val), this.snapPoint, dampingForMaster, wasRunMaster)),
-            set(this.masterVelocity, 0)
-          ]),
-          cond(eq(this.panState, State.END), set(justEndedIfEnded, 0)),
-          set(this.preventDecaying, 1),
-          0
-        ], [
-          set(this.preventDecaying, 0),
-          limitedVal
-        ])
-    ]);
-  }
-
-  panRef = React.createRef();
-
-  snapTo = index => {
-    if (!this.props.enabledManualSnapping) {
-      return
-    }
-    this.manuallySetValue.setValue(this.state.snapPoints[this.state.propsToNewIncides[index]])
-    this.isManuallySetValue.setValue(1);
-  }
-
-  handleLayoutHeader = ({
-                          nativeEvent: {
-                            layout: {
-                              height: heightOfHeader
-                            }
-                          }
-                        }) => {
-    this.state.heightOfHeaderAnimated.setValue(heightOfHeader);
-    this.setState({ heightOfHeader });
-  };
-
-  handleLayoutContent = ({
-                           nativeEvent: {
-                             layout: {
-                               height
-                             }
-                           }
-                         }) => this.state.heightOfContent.setValue(height - this.props.snapPoints[0]);
-
-
-  static getDerivedStateFromProps(props, state) {
-    let snapPoints;
-    const sortedPropsSnapPints = props.snapPoints.map((s, i) => {
-      if (typeof s === 'number') {
-        return { val: s, ind:i }
-      } else if (typeof s === 'string') {
-        return { val: Number(s.split('%')[0]) * height / 100, ind: i }
-      } else {
-        // exception
-      }
-    }).sort(({ val: a }, { val: b }) => a < b)
-    if (state && state.snapPoints) {
-      state.snapPoints.forEach((s, i) => s.setValue(sortedPropsSnapPints[0].val - sortedPropsSnapPints[i].val))
-      snapPoints = state.snapPoints
-    } else {
-      snapPoints = sortedPropsSnapPints.map(p => new Value(sortedPropsSnapPints[0].val - p.val))
-    }
-
-    const propsToNewIncides = {};
-    sortedPropsSnapPints.forEach(({ ind }, i) => propsToNewIncides[ind] = i )
-
-    const { initialSnap } = props;
-
-
-    return {
-      init: sortedPropsSnapPints[0].val - sortedPropsSnapPints[propsToNewIncides[initialSnap]].val,
-      propsToNewIncides,
-      heightOfHeaderAnimated: (state && state.heightOfHeaderAnimated) || new Animated.Value(0),
-      heightOfContent: (state && state.heightOfContent) || new Animated.Value(0),
-      initSnap: height - props.snapPoints[0],
-      snapPoints
-    };
-  }
-
-  master = React.createRef();
-
   render() {
+    const data = Object.keys(SCREENS).map(key => ({ key }));
     return (
-        <Animated.View style={{
-          width: '100%',
-          position: 'absolute',
-          zIndex: 100,
-          transform: [
-            {
-              translateY: this.translateMaster
-            },
-            {
-              translateY: this.state.initSnap
-            }
-          ]
-        }}>
-          <PanGestureHandler
-            enabled={this.props.enabledGestureInteraction}
-            ref={this.master}
-            waitFor={this.panRef}
-            onGestureEvent={this.handleMasterPan}
-            onHandlerStateChange={this.handleMasterPan}
-          >
-            <Animated.View
-              style={{
-                zIndex: 101
-              }}
-              onLayout={this.handleLayoutHeader}
-            >
-              {this.props.renderHeader && this.props.renderHeader()}
-            </Animated.View>
-          </PanGestureHandler>
-          <View
-            style={{
-              height: this.props.snapPoints[0] - this.state.heightOfHeader,
-              overflow: 'hidden'
-            }}
-          >
-
-            <PanGestureHandler
-              enabled={this.props.enabledGestureInteraction}
-              waitFor={this.master}
-              ref={this.panRef}
-              onGestureEvent={this.handlePan}
-              onHandlerStateChange={this.handlePan}
-            >
-              <Animated.View>
-                <TapGestureHandler
-                  enabled={this.props.enabledGestureInteraction}
-                  onHandlerStateChange={this.handleTap}
-                >
-                  <Animated.View
-                    style={{
-                      width: '100%',
-                      transform: [
-                        { translateY: this.Y }
-                      ]
-                    }}
-                    onLayout={this.handleLayoutContent}
-                  >
-                    {this.props.renderContent && this.props.renderContent()}
-                  </Animated.View>
-                </TapGestureHandler>
-              </Animated.View>
-            </PanGestureHandler>
-            <Animated.Code
-              exec={onChange(this.tapState, cond(eq(this.tapState, State.BEGAN), stopClock(this.decayClock)))}/>
-          </View>
-        </Animated.View>
+      <FlatList
+        style={styles.list}
+        data={data}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={props => (
+          <MainScreenItem
+            {...props}
+            onPressItem={({ key }) => this.props.navigation.navigate(key)}
+          />
+        )}
+      />
     );
   }
 }
 
-export default class Example extends React.Component {
-  renderInner = () => (
-    <View style={styles.panel}>
-      <Text style={styles.panelTitle}>San Francisco Airport</Text>
-      <Text style={styles.panelSubtitle}>
-        International Airport - 40 miles away
-      </Text>
-      <View style={styles.panelButton}>
-        <Text style={styles.panelButtonTitle}>Directions</Text>
-      </View>
-      <View style={styles.panelButton}>
-        <Text style={styles.panelButtonTitle}>Search Nearby</Text>
-      </View>
-      <Image
-        style={styles.photo}
-        source={require('./assets/airport-photo.jpg')}
-      />
-    </View>
-  );
+const ItemSeparator = () => <View style={styles.separator} />;
 
-  renderHeader = () => (
-    <View style={styles.header}>
-      <View style={styles.panelHeader}>
-        <View style={styles.panelHandle} />
-      </View>
-    </View>
-  )
-
-  bs = React.createRef()
-
+class MainScreenItem extends React.Component {
+  _onPress = () => this.props.onPressItem(this.props.item);
   render() {
+    const { key } = this.props.item;
     return (
-      <View style={styles.container}>
-        <BottomSheetBehavior
-          ref={this.bs}
-          snapPoints = {[450, 300, 100, 0]}
-          renderContent = {this.renderInner}
-          renderHeader = {this.renderHeader}
-          initialSnap = {1}
-        />
-        <TouchableWithoutFeedback onPress={() => this.bs.current.snapTo(0)}>
-          <Image style={styles.map} source={require('./assets/map-bg.jpg')} />
-        </TouchableWithoutFeedback>
-
-      </View>
-    )
+      <TouchableOpacity style={styles.button} onPress={this._onPress}>
+        <Text style={styles.buttonText}>{SCREENS[key].title || key}</Text>
+      </TouchableOpacity>
+    );
   }
 }
 
-const IMAGE_SIZE = 200;
+const ExampleApp = createAppContainer(createStackNavigator(
+  {
+    Main: { screen: MainScreen },
+    ...SCREENS,
+  },
+  {
+    initialRouteName: 'Main',
+  }
+));
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
+    backgroundColor: '#EFEFF4',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DBDBE0',
+  },
+  buttonText: {
+    backgroundColor: 'transparent',
+  },
+  button: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  box: {
-    width: IMAGE_SIZE,
-    height: IMAGE_SIZE,
-  },
-  panelContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-  panel: {
-    height: 600,
-    padding: 20,
-    backgroundColor: '#f7f5eee8',
-
-  },
-  header: {
-    backgroundColor: '#f7f5eee8',
-    shadowColor: '#000000',
-    paddingTop: 20,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  panelHeader: {
+    height: 60,
+    padding: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-  },
-  panelHandle: {
-    width: 40,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#00000040',
-    marginBottom: 10,
-  },
-  panelTitle: {
-    fontSize: 27,
-    height: 35,
-  },
-  panelSubtitle: {
-    fontSize: 14,
-    color: 'gray',
-    height: 30,
-    marginBottom: 10,
-  },
-  panelButton: {
-    padding: 20,
-    borderRadius: 10,
-    backgroundColor: '#318bfb',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  panelButtonTitle: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  photo: {
-    width: '100%',
-    height: 225,
-    marginTop: 30,
-  },
-  map: {
-    height: '100%',
-    width: '100%',
+    backgroundColor: '#fff',
   },
 });
 
+export default ExampleApp;
